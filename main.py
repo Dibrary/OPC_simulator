1d88d38bb296c78b8f28dddc6af3e4ab84c317b0
Dibrary <kkdh828@hanmail.net>
2021-11-21 12:43:01 +0900

    211121 xml tag별로, 갯수를 확인하는 코드 추가. (해당 tag 담는 자료구조 갯수 initial용)

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ class Main(QDialog):
 
     def setting(self): # xml파일 파싱을 자동으로 해 놓는 코드.
         root = (etree.parse("taglist.xml")).getroot()
+        tag_group_count = []
+        for idx, k in enumerate(root.iter()):
+            if k.tag == "tag":
+                tag_group_count.append(idx)
+            if len(tag_group_count) == 2:
+                break
+        tag_count = tag_group_count[1]-tag_group_count[0]-1
+        QMessageBox.information(self, "알림", str(tag_count))
+
         analyzer = []
         for country in root.iter("tag"):  # 우선 반복문을 통해 tag의 요소들을 '순서대로' 배열에 넣자.
             analyzer.append(country.findtext("analyzer"))
